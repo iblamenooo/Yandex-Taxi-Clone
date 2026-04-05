48f43f698214363f2a9f38f4c256f81f306758f9
Nurtore Kambar <nurtorekambar21@gmail.com>
2026-04-05 23:50:49 +0500

    Initial Commit

---
+++ Yandex Taxi Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yandex Taxi Clone
+//
+//  Created by Nurtore on 05.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
