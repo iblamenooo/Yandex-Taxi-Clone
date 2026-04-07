c9ca7215dd6a7d5647e2cfea569142e2e660b00a
Nurtore Kambar <nurtorekambar21@gmail.com>
2026-04-07 11:10:49 +0500

    Initial changes

---
+++ View/Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extensions.swift
+//  Yandex Taxi Clone
+//
+//  Created by Nurtore on 06.04.2026.
+//
+import UIKit
+import MapKit
+
+extension UIView {
+    
+}
---
+++ View/LoginController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginController.swift
+//  Yandex Taxi Clone
+//
+//  Created by Nurtore on 06.04.2026.
+//
+
+import UIKit
+import Firebase
+
+class LoginController:UIViewController {
+    private let label:UILabel = {
+        let label = UILabel()
+        label.text = "Yandex Taxi"
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
