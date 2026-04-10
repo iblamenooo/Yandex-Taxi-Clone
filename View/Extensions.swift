d6671cfc0d28e2da9e9ed7e96886caf1cdc9c0ee
Nurtore Kambar <nurtorekambar21@gmail.com>
2026-04-11 00:11:43 +0500

    Minor changes to Login view

--- View/Extensions.swift
+++ View/Extensions.swift
@@ -11,9 +11,10 @@ extension UIView {
     //переиспользуемый UI Builder Method
     func containterInputView(image:UIImage, textField:UITextField? = nil, segmentedControl: UISegmentedControl? = nil) -> UIView {
         let view = UIView()
-        let imageView = UIImageView()
+        
+        let imageView = UIImageView() //иконка
         imageView.image = image
-        imageView.alpha = 0.87
+        imageView.alpha = 0.87 //альфа делает иконку слегка прозрачной
         view.addSubview(imageView)
         
         if let textField = textField {
@@ -33,11 +34,85 @@ extension UIView {
             sc.anchor(left:view.leftAnchor, right:view.rightAnchor, paddingLeft:8, paddingRight:8)
         }
         
-        let separatorView = UIView()
+        let separatorView = UIView()  //для нижной линии ______________ что то вроде этого
         separatorView.backgroundColor = .lightGray
         view.addSubview(separatorView)
         separatorView.anchor(left:view.leftAnchor, botttom:view.bottomAnchor, right:view.rightAnchor, paddingLeft:8, height:0.75 )
         
         return view
     }
+    //custom Auto Layout helper
+    func anchor(top:NSLayoutYAxisAnchor? = nil, //вместо долгого NSLayout.constraint можно просто написать view.anchor() это енкапсуляция layout logic
+                left:NSLayoutXAxisAnchor? = nil,
+                bottom:NSLayoutYAxisAnchor? = nil,
+                right:NSLayoutXAxisAnchor? = nil,
+                paddingTop:CGFloat = 0,
+                paddingLeft:CGFloat = 0,
+                paddingBottom:CGFloat = 0,
+                paddingRight:CGFloat = 0,
+                width:CGFloat? = nil,
+                height:CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant:paddingTop).isActive = true
+        }
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant:paddingLeft).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant:-paddingBottom).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view:UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func centerY(inView view:UIView, leftAnchor:NSLayoutXAxisAnchor?=nil, paddingLeft:CGFloat = 0, constant:CGFloat = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        if let left = leftAnchor {
+            anchor(left:left, paddingLeft:paddingLeft)
+        }
+    }
+    
+    func setDimensions(height:CGFloat, width:CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    func addShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.55
+        layer.shadowOffset = CGSize(width:0.5, height:0.5)
+        layer.masksToBounds = false
+    }
 }
+
+extension UITextField {
+    func textField(withPlaceholder placeholder:String, isSecureTextEntry:Bool) -> UITextField {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize:16)
+        tf.textColor = .black
+        tf.keyboardAppearance = .dark
+        tf.isSecureTextEntry = isSecureTextEntry
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        return tf
+    }
+}
+
--- View/LoginController.swift
+++ View/LoginController.swift
@@ -13,9 +13,29 @@ class LoginController:UIViewController {
         let label = UILabel()
         label.text = "Yandex Taxi"
         label.textColor = UIColor(white: 1, alpha: 0.8)
+        
         return label
     }()
     
+    private lazy var emailContainerView:UIView = {
+        let view = UIView().containterInputView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private lazy var passwordContainerView:UIView = {
+        let view = UIView().containterInputView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private let emailTextField:UITextField = {
+        let tf = UITextField()
+        return tf.textField(withPlaceholder: "Email", isSecureTextEntry: false)
+    }
+    
+    private let passwordTextField:UITextField = {
+        let tf = UITextField()
+        return tf.textField(withPlaceholder: "Password", isSecureTextEntry: true)
+    }
     
     
     
